f3c1b11a93234febdab851639b32f9158c4fe865
Vedant Singh <vedantpsj1@gmail.com>
2023-04-24 19:11:19 +0530

    changes for create/edit user

--- pages/userForm.tsx
+++ pages/userForm.tsx
@@ -1,45 +1,81 @@
-import InputField from "./inputField"
-import closeIcon from '../assets/images/close.svg';
-import Image from 'next/image'
+import InputField from "./inputField";
+import closeIcon from "../assets/images/close.svg";
+import Image from "next/image";
+import { useForm } from "react-hook-form";
+import { USER } from "@/types/user";
 
-export default function UserForm() {
-    return (
-        <div className="relative z-10" aria-labelledby="modal-title" role="dialog" aria-modal="true">
-            <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"></div>
-            <div className="fixed inset-0 z-10 overflow-y-auto">
-                <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
-                    <div className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg w-[100%]">
-                        <div className="bg-slate-200 text-black px-6 py-3 flex justify-between">
-                            <span>Update Details</span>
+export default function UserForm({ userData }: { userData?: USER }) {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
 
-                            <span className="cursor-pointer">
-                                <Image src={closeIcon} alt='Close'/>
-                            </span>
-                        </div>
-                        <div className="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">   
-                            <form>
-                                <div className="grid gap-4 mb-4 md:grid-cols-2">
-                                    <InputField placeholder="Email" type="email" required={true}/>
-                                    <InputField placeholder="Username" required={true}/>
-                                </div>
+  const handleFormSubmit = (data: any) => {
+    console.log("------------ data", data);
+  };
 
-                                <div className="grid gap-4 mb-6 md:grid-cols-3">
-                                    <InputField placeholder="Street"/>
-                                    <InputField placeholder="Postal Code"/>
-                                    <InputField placeholder="Country Code"/>
-                                </div>
-                                <div className="text-right">
-                                    <button type="submit" className="text-white bg-slate-600 hover:bg-slate-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
-                                </div>
-                            </form>
-                        </div>
-                    </div>
-                </div>
+  return (
+    <div
+      className="relative z-10"
+      aria-labelledby="modal-title"
+      role="dialog"
+      aria-modal="true"
+    >
+      <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"></div>
+      <div className="fixed inset-0 z-10 overflow-y-auto">
+        <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+          <div className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg w-[100%]">
+            <div className="bg-slate-200 text-black px-6 py-3 flex justify-between">
+              <span>Update Details</span>
+
+              <span className="cursor-pointer">
+                <Image src={closeIcon} alt="Close" />
+              </span>
             </div>
-        </div>
-    );
-}
-function useState(arg0: boolean): [any, any] {
-    throw new Error("Function not implemented.");
-}
+            <div className="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
+              <form onSubmit={handleSubmit(handleFormSubmit)}>
+                <div className="grid gap-4 mb-4 md:grid-cols-2">
+                  <InputField
+                    formRegister={register("email")}
+                    placeholder="Email"
+                    type="email"
+                    required={true}
+                  />
+                  <InputField
+                    formRegister={register("username")}
+                    placeholder="Username"
+                    required={true}
+                  />
+                </div>
 
+                <div className="grid gap-4 mb-6 md:grid-cols-3">
+                  <InputField
+                    formRegister={register("street")}
+                    placeholder="Street"
+                  />
+                  <InputField
+                    formRegister={register("postalCode")}
+                    placeholder="Postal Code"
+                  />
+                  <InputField
+                    formRegister={register("countryCode")}
+                    placeholder="Country Code"
+                  />
+                </div>
+                <div className="text-right">
+                  <button
+                    type="submit"
+                    className="text-white bg-slate-600 hover:bg-slate-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                  >
+                    Submit
+                  </button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
--- pages/users.tsx
+++ pages/users.tsx
@@ -4,17 +4,17 @@ import { Inter } from "next/font/google";
 import { useEffect, useState } from "react";
 import UserForm from "./userForm";
 import Loader from "./loader";
-import Image from 'next/image'
-import deleteIcon from '../assets/images/delete.svg';
-import editIcon from '../assets/images/edit.svg';
+import Image from "next/image";
+import deleteIcon from "../assets/images/delete.svg";
+import editIcon from "../assets/images/edit.svg";
 
 const inter = Inter({ subsets: ["latin"] });
 
 const Users = () => {
   let [users, setUsers] = useState([]);
   let [loading, setLoading] = useState(true);
-  const [editDetail, setEditDetail] = useState(false);
-  
+  const [openUserModal, toggleUserModal] = useState(false);
+
   useEffect(() => {
     getAllUsers();
   }, []);
@@ -82,65 +82,88 @@ const Users = () => {
 
   const renderUsersTable = () => {
     return (
-        <>
+      <>
+        <button
+          onClick={() => toggleUserModal(true)}
+          className="text-white bg-slate-600 hover:bg-slate-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+        >
+          Create User
+        </button>
         <div className="overflow-x-auto">
-            <table className="bg-white bg-opacity-50 my-[20px] min-w-[900px] table-auto w-[100%]">
+          <table className="bg-white bg-opacity-50 my-[20px] min-w-[900px] table-auto w-[100%]">
             <thead>
-            <tr>
-                <th className="bg-slate-300 px-4 py-2 text-left text-gray-950">Email</th>
-                <th className="bg-slate-300 px-4 py-2 text-left text-gray-950 border-s">Username</th>
-                <th className="bg-slate-300 px-4 py-2 text-left text-gray-950 border-s">Address</th>
-                <th className="bg-slate-300 px-4 py-2 text-left text-gray-950 border-s">Edit/Delete</th>
-            </tr>
+              <tr>
+                <th className="bg-slate-300 px-4 py-2 text-left text-gray-950">
+                  Email
+                </th>
+                <th className="bg-slate-300 px-4 py-2 text-left text-gray-950 border-s">
+                  Username
+                </th>
+                <th className="bg-slate-300 px-4 py-2 text-left text-gray-950 border-s">
+                  Address
+                </th>
+                <th className="bg-slate-300 px-4 py-2 text-left text-gray-950 border-s">
+                  Edit/Delete
+                </th>
+              </tr>
             </thead>
             <tbody>
-                {new Array(6).fill(0).map((val, index) => (
-                    <tr>
-                    <td className="border px-4 py-2">test@email.com</td>
-                    <td className="border px-4 py-2">Testww3</td>
-                    <td className="border px-4 py-2 w-[400px] text-[90%]">
-                        <div className="flex">
-                            <span className="text-black w-[125px] pr-[10px]">Street:</span>
-                            <span className="w-[calc(100%-125px)]">121</span>
-                        </div>
-                        <div className="flex">
-                            <span className="text-black w-[125px] pr-[10px]">City:</span>
-                            <span className="w-[calc(100%-125px)]">City Name</span>
-                        </div>
-                        <div className="flex">
-                            <span className="text-black w-[125px] pr-[10px]">Postal Code:</span>
-                            <span className="w-[calc(100%-125px)]">122322</span>
-                        </div>
-                        <div className="flex">
-                            <span className="text-black w-[125px] pr-[10px]">Country Code:</span>
-                            <span className="w-[calc(100%-125px)]">+893</span>
-                        </div>
-                    </td>
-                    <td className="border px-4 py-2 w-[100px]">
-                        <div className="flex items-center justify-center">
-                            <span className="cursor-pointer text-green-600" onClick={()=> setEditDetail(true)}>
-                                <Image src={editIcon} alt='Edit'/>
-                            </span>
-        
-                            <span className="ml-[10px] cursor-pointer text-red-600">
-                                <Image src={deleteIcon} alt='Delete'/>
-                           </span>
-                        </div>
-                    </td>
+              {new Array(6).fill(0).map((val, index) => (
+                <tr>
+                  <td className="border px-4 py-2">test@email.com</td>
+                  <td className="border px-4 py-2">Testww3</td>
+                  <td className="border px-4 py-2 w-[400px] text-[90%]">
+                    <div className="flex">
+                      <span className="text-black w-[125px] pr-[10px]">
+                        Street:
+                      </span>
+                      <span className="w-[calc(100%-125px)]">121</span>
+                    </div>
+                    <div className="flex">
+                      <span className="text-black w-[125px] pr-[10px]">
+                        City:
+                      </span>
+                      <span className="w-[calc(100%-125px)]">City Name</span>
+                    </div>
+                    <div className="flex">
+                      <span className="text-black w-[125px] pr-[10px]">
+                        Postal Code:
+                      </span>
+                      <span className="w-[calc(100%-125px)]">122322</span>
+                    </div>
+                    <div className="flex">
+                      <span className="text-black w-[125px] pr-[10px]">
+                        Country Code:
+                      </span>
+                      <span className="w-[calc(100%-125px)]">+893</span>
+                    </div>
+                  </td>
+                  <td className="border px-4 py-2 w-[100px]">
+                    <div className="flex items-center justify-center">
+                      <span
+                        className="cursor-pointer text-green-600"
+                        onClick={() => toggleUserModal(true)}
+                      >
+                        <Image src={editIcon} alt="Edit" />
+                      </span>
+
+                      <span className="ml-[10px] cursor-pointer text-red-600">
+                        <Image src={deleteIcon} alt="Delete" />
+                      </span>
+                    </div>
+                  </td>
                 </tr>
-                ))} 
+              ))}
             </tbody>
-        </table>
-      </div>
-      {editDetail && <UserForm/>}
+          </table>
+        </div>
+        {openUserModal && <UserForm userData={users[0]} />}
       </>
     );
   };
 
   if (loading) {
-    return (
-        <Loader/>
-    );
+    return <Loader />;
   }
   return (
     <main className={`container mx-auto px-4 ${inter.className}`}>
